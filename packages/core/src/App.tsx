ca0804a7a6d7ff9785dc709588151e289fda3612
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-03-19 01:07:48 +0900

    clean up srcs

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
-
-export default App
+export const App = () => (
+  <div>
+    <h1>App</h1>
+    <p>Hello!</p>
+    <React.Suspense fallback='Loading ...'>
+      <p>Plugin 1</p>
+    </React.Suspense>
+  </div>
+)
