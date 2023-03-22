c25d431d19c4d8d941421e9e07b25ae0865f386d
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-03-22 21:35:20 +0900

    stop using type cast with importRemote

--- packages/core/src/dynamic-loading.ts
+++ packages/core/src/dynamic-loading.ts
@@ -14,7 +14,7 @@ export async function dynamicLoading() {
   console.log('Loading remote modules dynamically ...')
   await Promise.all(
     remotes.map(async remote => {
-      const { plugin } = (await importRemote(remote)) as { plugin: Plugin }
+      const { plugin } = await importRemote<{ plugin: Plugin }>(remote)
       plugin()
     }),
   )
